044483f2f86515eb158c5b86c41981c5b27e79ee
Alibek Omarov <a1ba.omarov@gmail.com>
2020-10-15 17:08:01 +0300

    target: e2k: add cpu definitions

---
+++ target/e2k/cpu-qom.h
@@ -0,0 +1,48 @@
+/*
+ * QEMU E2K CPU
+ *
+ * Copyright (c) 2012 SUSE LINUX Products GmbH
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>
+ */
+#ifndef QEMU_E2K_CPU_QOM_H
+#define QEMU_E2K_CPU_QOM_H
+
+#include "hw/core/cpu.h"
+#include "qom/object.h"
+
+#define TYPE_E2K_CPU "e2k-cpu"
+
+OBJECT_DECLARE_TYPE(E2KCPU, E2KCPUClass,
+                    E2K_CPU)
+
+/**
+ * E2KCPUClass:
+ * @parent_realize: The parent class' realize handler.
+ * @parent_reset: The parent class' reset handler.
+ *
+ * A E2K CPU model.
+ */
+struct E2KCPUClass {
+    /*< private >*/
+    CPUClass parent_class;
+    /*< public >*/
+
+    DeviceRealize parent_realize;
+    DeviceReset parent_reset;
+};
+
+
+#endif
---
+++ target/e2k/cpu.h
@@ -0,0 +1,44 @@
+#ifndef E2K_CPU_H
+#define E2K_CPU_H
+
+#include "qemu/bswap.h"
+#include "cpu-qom.h"
+#include "exec/cpu-defs.h"
+
+struct CPUE2KState {
+    
+    /* register file */ 
+    uint64_t gregs[32]; /* general registers */
+    uint64_t pregs[224]; /* procedure regs, access through wreg */
+    
+    /* control registers */
+    uint64_t ctpr1; // Control Transfer Preparation Register (CTPR)
+    uint64_t ctpr2;
+    uint64_t ctpr3;
+    
+    /* special registers */
+    uint64_t *wreg; /* pointer to current window */
+    uint32_t br;    /* base register offset, max 128 */
+    uint64_t ip, nip; /* instruction address, next instruction address */
+    
+    uint32_t pfpfr; // Packed Floating Point Flag Register (PFPFR)
+    uint32_t fpcr; // Floating point control register (FPCR)
+    uint32_t fpsr; // Floating point state register (FPSR)
+};
+
+/**
+ * E2KCPU:
+ * @env: #CPUE2KState
+ *
+ * An Elbrus CPU.
+ */
+struct E2KCPU {
+    /*< private >*/
+    CPUState parent_obj;
+    /*< public >*/
+
+    CPUNegativeOffsetState neg;
+    CPUE2KState env;
+};
+
+#endif
