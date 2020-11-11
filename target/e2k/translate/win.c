2d97925dab52ea7cec5700f253466b9b9d8a2dbb
Denis Drakhnya <numas13@gmail.com>
2020-11-11 22:45:55 +0200

    target: e2k: Reorg control flow.

--- target/e2k/translate/win.c
+++ target/e2k/translate/win.c
@@ -5,19 +5,19 @@
 #include "exec/translator.h"
 #include "translate.h"
 
-static inline void gen_abn_inc(TCGCond cond)
+static inline void gen_abn_inc(DisasContext *dc, TCGCond cond)
 {
     TCGv_i32 t0 = tcg_temp_new_i32();
     TCGv_i32 t1 = tcg_temp_new_i32();
-    TCGv_i32 zero = tcg_const_i32(0);
+    TCGv_i32 one = tcg_const_i32(1);
 
     tcg_gen_addi_i32(t0, e2k_cs.rcur, 2);
     e2k_gen_wrap_i32(t1, t0, e2k_cs.rsz);
     tcg_gen_movcond_i32(cond, e2k_cs.rcur,
-        e2k_cs.is_jmp, zero,
+        dc->jmp.cond, one,
         t1, e2k_cs.rcur);
 
-    tcg_temp_free_i32(zero);
+    tcg_temp_free_i32(one);
     tcg_temp_free_i32(t1);
     tcg_temp_free_i32(t0);
 }
@@ -32,9 +32,9 @@ void e2k_win_commit(DisasContext *dc)
 //    unsigned int alc = (ss & 0x00030000) >> 16;
 
     if (GET_BIT(ss, 21)) {
-        gen_abn_inc(TCG_COND_NE);
+        gen_abn_inc(dc, TCG_COND_EQ);
     }
     if (GET_BIT(ss, 22)) {
-        gen_abn_inc(TCG_COND_EQ);
+        gen_abn_inc(dc, TCG_COND_NE);
     }
 }
