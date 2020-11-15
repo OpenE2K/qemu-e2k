0198775b6703b97fd90dbb148b70001acda9a0b7
Denis Drakhnya <numas13@gmail.com>
2020-11-15 17:05:27 +0200

    target: e2k: Add basic call (without saving regs).

--- target/e2k/helper.c
+++ target/e2k/helper.c
@@ -14,21 +14,132 @@ void helper_raise_exception(CPUE2KState *env, int tt)
     cpu_loop_exit(cs);
 }
 
-void helper_call(CPUE2KState *env, target_ulong ctpr, target_ulong cond)
+static inline void do_call(CPUE2KState *env, target_ulong ctpr)
 {
-    int tag = GET_FIELD(ctpr, CTPR_TAG_OFF, CTPR_TAG_END);
+    unsigned int offset, wbs, wsz, new_wbs, new_wsz;
+    uint64_t *pcsp = (uint64_t *)
+        GET_FIELD(env->pcsp_lo, PCSP_LO_BASE_OFF, PCSP_LO_BASE_END);
+
+    wbs = e2k_state_wbs_get(env);
+    wsz = e2k_state_wsz_get(env);
+    new_wbs = (wbs + env->call_wbs) % WREGS_SIZE;
+    new_wsz = wsz - env->call_wbs;
+
+    /* save procedure chain */
+    offset = (env->pcsp_hi & GEN_MASK(0, 31)) / 8;
+    pcsp[offset + 0] = env->pf;
+    pcsp[offset + 1] = env->nip;
+    pcsp[offset + 2] = env->cr1_lo;
+    pcsp[offset + 3] = env->cr1_hi;
+    /* TODO: check stack overflow */
+    env->pcsp_hi = env->pcsp_hi + 32;
+
+    /* TODO: check if call_wbs is less or equal then wsz */
+    e2k_state_wbs_set(env, new_wbs);
+    e2k_state_wsz_set(env, new_wsz);
+    /* TODO: check rbs/rsz after call */
+    env->ip = GET_FIELD(ctpr, CTPR_BASE_OFF, CTPR_BASE_END);
+}
+
+static void do_return(CPUE2KState *env)
+{
+    unsigned int offset = env->pcsp_hi & GEN_MASK(0, 31);
+    uint64_t *pcsp = (uint64_t *) (GET_FIELD(env->pcsp_lo, PCSP_LO_BASE_OFF,
+        PCSP_LO_BASE_END) + offset - 32);
+//    uint64_t *psp = (uint64_t *) GET_FIELD(env->psp_lo, PSP_LO_BASE_OFF,
+//        PSP_LO_BASE_END);
+
+    env->pf = pcsp[0];
+    env->ip = pcsp[1];
+    env->nip = env->ip + 8;
+    env->cr1_lo = pcsp[2];
+    env->cr1_hi = pcsp[3];
+    /* TODO: check stack overflow */
+    env->pcsp_hi = env->pcsp_hi - 32;
+
+    // TODO: restore regs
+}
+
+void helper_call(CPUE2KState *env, target_ulong cond, target_ulong ctpr)
+{
+    unsigned int i = 0;
+
     if (!cond) {
+        env->ip = env->nip;
         return;
     }
-    if (tag == CTPR_TAG_SDISP) {
+
+    for (i = 0; i < 4; i++) {
+        env->ctprs[i] = 0;
+    }
+
+    switch (GET_FIELD(ctpr, CTPR_TAG_OFF, CTPR_TAG_END)) {
+    case CTPR_TAG_SDISP: {
         CPUState *cs = env_cpu(env);
 
+        env->ip = env->nip;
         cs->exception_index = E2K_EXCP_SYSCALL;
         cpu_loop_exit(cs);
-    } else {
-        /* TODO: call */
-        abort();
+        break;
     }
+    case CTPR_TAG_DISP: {
+        do_call(env, ctpr);
+        break;
+    }
+    default:
+        /* TODO: exception */
+        abort();
+        break;
+    }
+}
+
+/* FIXME: combine with helper_call? */
+void helper_jump(CPUE2KState *env, target_ulong cond, target_ulong ctpr)
+{
+    if (!cond) {
+        env->ip = env->nip;
+        return;
+    }
+
+    switch (GET_FIELD(ctpr, CTPR_TAG_OFF, CTPR_TAG_END)) {
+    case CTPR_TAG_DISP: {
+        env->ip = GET_FIELD(ctpr, CTPR_BASE_OFF, CTPR_BASE_END);
+        break;
+    }
+    case CTPR_TAG_RETURN:
+        do_return(env);
+        break;
+    default:
+        /* TODO: exception */
+        abort();
+        break;
+    }
+}
+
+void helper_setwd(CPUE2KState *env, uint32_t lts0)
+{
+    int wsz = GET_FIELD(lts0, 5, 11);
+    /* TODO: store nfx */
+//    int nfx = GET_BIT(lts0, 4);
+
+    /* TODO: push wregs */
+
+    e2k_state_wsz_set(env, wsz);
+
+    if (env->version >= 3) {
+//        int dbl = GET_BIT(lts0, 3);
+        /* TODO: store dbl */
+    }
+}
+
+void helper_setbn(CPUE2KState *env, uint32_t cs1)
+{
+    env->cr1_hi = SET_FIELD(
+        env->cr1_hi,
+        GET_FIELD(cs1, 0, 17),
+        CR1_HI_BR_OFF + BR_BN_OFF,
+        BR_BN_LEN
+    );
 }
 
 uint64_t helper_sxt(uint64_t x, uint64_t y)
@@ -62,4 +173,64 @@ void helper_debug_i32(uint32_t x)
 void helper_debug_i64(uint64_t x)
 {
     qemu_log_mask(LOG_UNIMP, "log %#lx\n", x);
+}
+
+uint64_t helper_state_reg_get(CPUE2KState *env, int reg)
+{
+    switch (reg) {
+    case 0x2c: /* %usd.hi */
+        return env->usd_hi;
+        break;
+    case 0x2d: /* %usd.lo */
+        return env->usd_lo;
+        break;
+    case 0x83: /* %lsr */
+        return env->lsr;
+        break;
+    default:
+        /* TODO: exception */
+        abort();
+        break;
+    }
+}
+
+void helper_state_reg_set(CPUE2KState *env, int reg, uint64_t val)
+{
+    switch (reg) {
+    case 0x2c: /* %usd.hi */
+        /* FIXME: user cannot write */
+        env->usd_hi = val;
+        break;
+    case 0x2d: /* %usd.lo */
+        /* FIXME: user cannot write */
+        env->usd_lo = val;
+        break;
+    case 0x83: /* %lsr */
+        env->lsr = val;
+        break;
+    default:
+        /* TODO: exception */
+        abort();
+        break;
+    }
+}
+
+void helper_save_rcur(CPUE2KState *env, int rcur)
+{
+    env->cr1_hi = SET_FIELD(
+        env->cr1_hi,
+        rcur / 2,
+        CR1_HI_BR_OFF + BR_RCUR_OFF,
+        BR_RCUR_LEN
+    );
+}
+
+void helper_save_pcur(CPUE2KState *env, int pcur)
+{
+    env->cr1_hi = SET_FIELD(
+        env->cr1_hi,
+        pcur,
+        CR1_HI_BR_OFF + BR_PCUR_OFF,
+        BR_PCUR_LEN
+    );
 }
--- target/e2k/translate/state.c
+++ target/e2k/translate/state.c
@@ -7,27 +7,18 @@ static void gen_preg_offset(TCGv_i64 ret, int reg)
 {
     assert(reg < 32);
 
-    TCGv_i32 br = tcg_temp_new_i32();
-    TCGv_i32 pcur = tcg_temp_new_i32();
-    TCGv_i32 psz = tcg_temp_new_i32();
     TCGv_i32 t0 = tcg_temp_new_i32();
     TCGv_i32 t1 = tcg_temp_new_i32();
     TCGv_i32 t2 = tcg_temp_new_i32();
 
-    e2k_gen_get_br(br);
-    tcg_gen_extract_i32(pcur, br, BR_PCUR_OFF, BR_PCUR_LEN);
-    tcg_gen_extract_i32(psz, br, BR_PSZ_OFF, BR_PSZ_LEN);
-    tcg_gen_addi_i32(t0, pcur, reg);
-    e2k_gen_wrap_i32(t1, t0, psz);
-    tcg_gen_shli_i32(t2, t1, 1);
+    tcg_gen_addi_i32(t0, e2k_cs.pcur, reg);
+    e2k_gen_wrap_i32(t1, t0, e2k_cs.psize);
+    tcg_gen_muli_i32(t2, t1, 2);
     tcg_gen_extu_i32_i64(ret, t2);
 
     tcg_temp_free_i32(t2);
     tcg_temp_free_i32(t1);
     tcg_temp_free_i32(t0);
-    tcg_temp_free_i32(psz);
-    tcg_temp_free_i32(pcur);
-    tcg_temp_free_i32(br);
 }
 
 static void gen_preg_clear(TCGv_i64 ret, TCGv_i64 offset)
@@ -47,15 +38,20 @@ static void gen_preg_clear(TCGv_i64 ret, TCGv_i64 offset)
 
 void e2k_gen_preg(TCGv_i64 ret, int reg)
 {
+    TCGv_i64 one = tcg_const_i64(1);
     TCGv_i64 t0 = tcg_temp_new_i64();
     TCGv_i64 t1 = tcg_temp_new_i64();
+    TCGv_i64 t2 = tcg_temp_new_i64();
 
     gen_preg_offset(t0, reg);
-    tcg_gen_shri_i64(t1, e2k_cs.pregs, reg * 2);
-    tcg_gen_andi_i64(ret, t1, 0x01);
+    tcg_gen_shl_i64(t1, one, t0);
+    tcg_gen_and_i64(t2, e2k_cs.pregs, t1);
+    tcg_gen_setcondi_i64(TCG_COND_NE, ret, t2, 0);
 
+    tcg_temp_free_i64(t2);
     tcg_temp_free_i64(t1);
     tcg_temp_free_i64(t0);
+    tcg_temp_free_i64(one);
 }
 
 TCGv_i64 e2k_get_preg(DisasContext *dc, int reg)
@@ -69,11 +65,12 @@ void e2k_gen_store_preg(int reg, TCGv_i64 val)
 {
     TCGv_i64 t0 = tcg_temp_new_i64();
     TCGv_i64 t1 = tcg_temp_new_i64();
-    TCGv_i64 t2 = tcg_const_i64(1);
+    TCGv_i64 t2 = tcg_temp_new_i64();
     TCGv_i64 t3 = tcg_temp_new_i64();
 
     gen_preg_offset(t0, reg);
     gen_preg_clear(t1, t0);
+    tcg_gen_setcondi_i64(TCG_COND_NE, t2, val, 0);
     tcg_gen_shl_i64(t3, t2, t0);
     tcg_gen_or_i64(e2k_cs.pregs, t1, t3);
 
@@ -83,23 +80,15 @@ void e2k_gen_store_preg(int reg, TCGv_i64 val)
     tcg_temp_free_i64(t0);
 }
 
-static inline void gen_wreg_offset(TCGv_i32 ret, int reg)
+static inline void gen_wreg_index(TCGv_i32 ret, int reg)
 {
-    assert(reg < 64);
-    // TODO: exception (wsz * 2) <= reg_index
-
     TCGv_i32 t0 = tcg_temp_new_i32();
-    TCGv_i32 t1 = tcg_temp_new_i32();
-    TCGv_i32 t2 = tcg_temp_new_i32();
-    TCGv_i32 t3 = tcg_const_i32(WREGS_SIZE);
+    TCGv_i32 t1 = tcg_const_i32(WREGS_SIZE);
 
-    tcg_gen_shli_i32(t0, e2k_cs.wbs, 1);    // t = wbs * 2
-    tcg_gen_addi_i32(t1, t0, reg);          // t = t + reg_index
-    e2k_gen_wrap_i32(t2, t1, t3);           // t = t % WIN_REGS_COUNT
-    tcg_gen_muli_i32(ret, t2, REG_SIZE);    // t = t * REG_SIZE_IN_BYTES
+    /* TODO: exception if overflow window size */
+    tcg_gen_addi_i32(t0, e2k_cs.woff, reg);
+    tcg_gen_remu_i32(ret, t0, t1);
 
-    tcg_temp_free_i32(t3);
-    tcg_temp_free_i32(t2);
     tcg_temp_free_i32(t1);
     tcg_temp_free_i32(t0);
 }
@@ -107,100 +96,76 @@ static inline void gen_wreg_offset(TCGv_i32 ret, int reg)
 static inline void gen_wreg_ptr(TCGv_ptr ret, int reg)
 {
     TCGv_i32 t0 = tcg_temp_new_i32();
-    TCGv_ptr t1 = tcg_temp_new_ptr();
-    gen_wreg_offset(t0, reg);
-    tcg_gen_ext_i32_ptr(t1, t0);
-    tcg_gen_add_ptr(ret, e2k_cs.win_ptr, t1);
-    tcg_temp_free_ptr(t1);
+    TCGv_i32 t1 = tcg_temp_new_i32();
+    TCGv_ptr t2 = tcg_temp_new_ptr();
+
+    gen_wreg_index(t0, reg);
+    tcg_gen_muli_i32(t1, t0, 8);
+    tcg_gen_ext_i32_ptr(t2, t1);
+    tcg_gen_add_ptr(ret, e2k_cs.wptr, t2);
+
+    tcg_temp_free_ptr(t2);
+    tcg_temp_free_i32(t1);
     tcg_temp_free_i32(t0);
 }
 
 TCGv_i64 e2k_get_wreg(DisasContext *dc, int reg)
 {
-    TCGv_i64 ret = e2k_get_temp_i64(dc);
-    TCGv_ptr ptr = tcg_temp_new_ptr();
-    gen_wreg_ptr(ptr, reg);
-    tcg_gen_ld_i64(ret, ptr, 0);
-    tcg_temp_free_ptr(ptr);
-    return ret;
+    TCGv_ptr t0 = tcg_temp_new_ptr();
+    TCGv_i64 t1 = e2k_get_temp_i64(dc);
+
+    gen_wreg_ptr(t0, reg);
+    tcg_gen_ld_i64(t1, t0, 0);
+
+    tcg_temp_free_ptr(t0);
+
+    return t1;
 }
 
 void e2k_gen_store_wreg(int reg, TCGv_i64 val)
 {
-    TCGv_ptr ptr = tcg_temp_new_ptr();
-    gen_wreg_ptr(ptr, reg);
-    tcg_gen_st_i64(val, ptr, 0);
-    tcg_temp_free_ptr(ptr);
+    TCGv_ptr t0 = tcg_temp_new_ptr();
+
+    gen_wreg_ptr(t0, reg);
+    tcg_gen_st_i64(val, t0, 0);
+
+    tcg_temp_free_ptr(t0);
 }
 
-static inline void gen_breg_start(TCGv_i32 ret, TCGv_i32 br)
+static inline void gen_breg_index(TCGv_i32 ret, int reg)
 {
-    TCGv_i32 rbs = tcg_temp_new_i32();
-    TCGv_i32 t0 = tcg_temp_new_i32();
-
-    tcg_gen_extract_i32(rbs, br, BR_RBS_OFF, BR_RBS_LEN);
-    tcg_gen_add_i32(t0, e2k_cs.wbs, rbs);
-    tcg_gen_shli_i32(ret, t0, 1);
-
-    tcg_temp_free_i32(t0);
-    tcg_temp_free_i32(rbs);
-}
-
-static inline void gen_breg_offset(TCGv_i32 ret, int reg)
-{
-    TCGv_i32 br = tcg_temp_new_i32();
-    TCGv_i32 rcur = tcg_temp_new_i32();
-    TCGv_i32 rsz = tcg_temp_new_i32();
     TCGv_i32 t0 = tcg_temp_new_i32();
     TCGv_i32 t1 = tcg_temp_new_i32();
     TCGv_i32 t2 = tcg_temp_new_i32();
     TCGv_i32 t3 = tcg_temp_new_i32();
-    TCGv_i32 t4 = tcg_temp_new_i32();
-    TCGv_i32 t5 = tcg_temp_new_i32();
-    TCGv_i32 t6 = tcg_temp_new_i32();
-    TCGv_i32 t7 = tcg_temp_new_i32();
+    TCGv_i32 t4 = tcg_const_i32(WREGS_SIZE);
 
-    e2k_gen_get_br(br);
-    /* TODO: exception: reg > (rsz * 2 + 2) */
-    /* t = (reg + rcur * 2) % (rsz * 2 + 2) */
-    tcg_gen_extract_i32(rcur, br, BR_RCUR_OFF, BR_RCUR_LEN);
-    tcg_gen_extract_i32(rsz, br, BR_RSZ_OFF, BR_RSZ_LEN);
-    tcg_gen_shli_i32(t0, rcur, 1);
-    tcg_gen_addi_i32(t1, t0, reg);
-    tcg_gen_addi_i32(t2, rsz, 1);
-    tcg_gen_shli_i32(t3, t2, 1);
-    e2k_gen_wrap_i32(t4, t1, t3);
+    tcg_gen_addi_i32(t0, e2k_cs.bcur, reg);
+    tcg_gen_remu_i32(t1, t0, e2k_cs.bsize);
+    tcg_gen_add_i32(t2, t1, e2k_cs.boff);
+    tcg_gen_add_i32(t3, t2, e2k_cs.woff);
+    tcg_gen_remu_i32(ret, t3, t4);
 
-    /* TODO: exceptioon: (reg + rbs * 2) > (wsz * 2) */
-    /* t = (t + wbs * 2 + rbs * 2) % 192 */
-    gen_breg_start(t5, br);
-    tcg_gen_add_i32(t6, t4, t5);
-    e2k_gen_wrapi_i32(t7, t6, WREGS_SIZE);
-
-    /* ret = t * 8 */
-    tcg_gen_muli_i32(ret, t7, REG_SIZE);
-
-    tcg_temp_free_i32(t7);
-    tcg_temp_free_i32(t6);
-    tcg_temp_free_i32(t5);
     tcg_temp_free_i32(t4);
     tcg_temp_free_i32(t3);
     tcg_temp_free_i32(t2);
     tcg_temp_free_i32(t1);
     tcg_temp_free_i32(t0);
-    tcg_temp_free_i32(rsz);
-    tcg_temp_free_i32(rcur);
-    tcg_temp_free_i32(br);
 }
 
 static inline void gen_breg_ptr(TCGv_ptr ret, int reg)
 {
     TCGv_i32 t0 = tcg_temp_new_i32();
-    TCGv_ptr t1 = tcg_temp_new_ptr();
-    gen_breg_offset(t0, reg);
-    tcg_gen_ext_i32_ptr(t1, t0);
-    tcg_gen_add_ptr(ret, e2k_cs.win_ptr, t1);
-    tcg_temp_free_ptr(t1);
+    TCGv_i32 t1 = tcg_temp_new_i32();
+    TCGv_ptr t2 = tcg_temp_new_ptr();
+
+    gen_breg_index(t0, reg);
+    tcg_gen_muli_i32(t1, t0, 8);
+    tcg_gen_ext_i32_ptr(t2, t1);
+    tcg_gen_add_ptr(ret, e2k_cs.wptr, t2);
+
+    tcg_temp_free_ptr(t2);
+    tcg_temp_free_i32(t1);
     tcg_temp_free_i32(t0);
 }
 
@@ -224,13 +189,12 @@ void e2k_gen_store_breg(int reg, TCGv_i64 val)
 
 TCGv_i64 e2k_get_greg(DisasContext *dc, int reg)
 {
-    // TODO: rotated gregs
-    assert(reg < 32);
+    assert(reg < GREGS_MAX + BGREGS_MAX);
     return e2k_cs.gregs[reg];
 }
 
 void e2k_gen_store_greg(int reg, TCGv_i64 val)
 {
-    // TODO: rotated gregs
+    assert(reg < GREGS_MAX + BGREGS_MAX);
     tcg_gen_mov_i64(e2k_cs.gregs[reg], val);
 }
