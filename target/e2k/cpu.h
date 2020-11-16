c7b6b7f8bf31a6387e0bd1eab0d1ac9741999523
Alibek Omarov <a1ba.omarov@gmail.com>
2020-11-16 21:54:23 +0300

    target: e2k: initial gdb support

--- target/e2k/cpu.h
+++ target/e2k/cpu.h
@@ -266,6 +266,8 @@ static inline void cpu_get_tb_cpu_state(CPUE2KState *env, target_ulong *pc,
 }
 
 int e2k_cpu_signal_handler(int host_signum, void *pinfo, void *puc);
+int e2k_cpu_gdb_read_register(CPUState *cs, GByteArray *mem_buf, int n);
+int e2k_cpu_gdb_write_register(CPUState *cs, uint8_t *mem_buf, int n);
 
 #define cpu_signal_handler e2k_cpu_signal_handler
 
