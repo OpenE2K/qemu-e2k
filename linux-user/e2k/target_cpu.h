c6ad6e65309d35dbf6d1205e57b0105898cdad45
Denis Drakhnya <numas13@gmail.com>
2020-11-28 10:08:22 +0200

    target: e2k: Update TODO messages.

--- linux-user/e2k/target_cpu.h
+++ linux-user/e2k/target_cpu.h
@@ -16,25 +16,25 @@ static inline void cpu_clone_regs_child(CPUE2KState *env, target_ulong newsp,
 static inline void cpu_clone_regs_parent(CPUE2KState *env, unsigned flags)
 {
     qemu_log_mask(LOG_UNIMP, "cpu_clone_regs_parent: unimplemented\n");
-    // TODO
+    // TODO: cpu_clone_regs_parent
 }
 
 static inline void cpu_set_tls(CPUE2KState *env, target_ulong newtls)
 {
     qemu_log_mask(LOG_UNIMP, "cpu_set_tls: unimplemented\n");
-    // TODO
+    // TODO: cpu_set_tls
 }
 
 static inline target_ulong cpu_get_tls(CPUE2KState *env)
 {
-    // TODO
+    // TODO: cpu_get_tls
     qemu_log_mask(LOG_UNIMP, "cpu_get_tls: unimplemented\n");
     return 0;
 }
 
 static inline abi_ulong get_sp_from_cpustate(CPUE2KState *env)
 {
-    return env->usd_lo & ((1UL << 48) - 1);
+    return extract64(env->usd_lo, 0, 48);
 }
 
 #endif /* E2K_TARGET_CPU_H */
