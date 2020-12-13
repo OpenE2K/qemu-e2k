37b4ee19b09b366c5cf38aebb38fa5a64bc69dfc
Denis Drakhnya <numas13@gmail.com>
2020-12-13 11:10:53 +0200

    target: e2k: Remove unneeded regs array in TCG state.

--- target/e2k/translate.h
+++ target/e2k/translate.h
@@ -41,7 +41,6 @@ typedef struct CPUE2KStateTCG {
     TCGv_i32 is_bp; /* breakpoint flag */
     TCGv_i32 wdbl;
     TCGv_i64 lsr;
-    TCGv_i64 regs[E2K_REG_COUNT];
     TCGv_ptr rptr; /* pointer to wregs */
     TCGv_ptr tptr; /* pointer to wtags */
     TCGv_i32 wd_base; /* holds wbs * 2 */
