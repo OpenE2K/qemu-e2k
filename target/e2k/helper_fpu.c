ca375bc45085b08cc4394ad254c9bd26fd9574b9
Denis Drakhnya <numas13@gmail.com>
2020-12-16 21:19:01 +0200

    target: e2k: Add basic FX instrs.

--- target/e2k/helper_fpu.c
+++ target/e2k/helper_fpu.c
@@ -5,6 +5,9 @@
 #include "qemu/host-utils.h"
 #include "exec/helper-proto.h"
 
+#define glue3(x, y, z) glue(glue(x, y), z)
+#define deref(x) *(x)
+
 static inline void fpu_set_exception(CPUE2KState *env, int mask)
 {
     env->fpsr.ef |= mask;
@@ -93,21 +96,21 @@ void e2k_update_fp_status(CPUE2KState *env)
 #define no_cvt(x) (x) // when function already returns in correct type
 
 #define GENERATE_CVT_FLOAT1_OP(name, from_t, to_t, size_from, size_to, func_from, func_to) \
-    uint##size_to##_t HELPER(name)(CPUE2KState *env, uint##size_from##_t x) \
+    size_to HELPER(name)(CPUE2KState *env, size_from x) \
     {\
         uint8_t old_flags = save_exception_flags(env); \
-        uint##size_to##_t z = func_to( from_t##_to_##to_t (func_from(x), &env->fp_status) );\
+        size_to z = func_to( glue3(from_t, _to_, to_t) (func_from(x), &env->fp_status) );\
         merge_exception_flags(env, old_flags); \
         return z; \
     }
 
 #define GENERATE_SIMPLE_FLOAT2_OPS_32_64(name, function) \
-    GENERATE_SIMPLE_FLOAT2_OP(name##s, function, 32) \
-    GENERATE_SIMPLE_FLOAT2_OP(name##d, function, 64)
+    GENERATE_SIMPLE_FLOAT2_OP(glue(name, s), function, 32) \
+    GENERATE_SIMPLE_FLOAT2_OP(glue(name, d), function, 64)
 
 #define GENERATE_CMP_FLOAT2_OPS_32_64(name, function, expr) \
-    GENERATE_CMP_FLOAT2_OP(name##s, function, expr, 32) \
-    GENERATE_CMP_FLOAT2_OP(name##d, function, expr, 64)
+    GENERATE_CMP_FLOAT2_OP(glue(name, s), function, expr, 32) \
+    GENERATE_CMP_FLOAT2_OP(glue(name, d), function, expr, 64)
         
 GENERATE_SIMPLE_FLOAT2_OPS_32_64(fadd, add)
 GENERATE_SIMPLE_FLOAT2_OPS_32_64(fsub, sub)
@@ -124,20 +127,57 @@ GENERATE_CMP_FLOAT2_OPS_32_64(fcmpnlt, lt, !)
 GENERATE_CMP_FLOAT2_OPS_32_64(fcmpuod, unordered,  )
 GENERATE_CMP_FLOAT2_OPS_32_64(fcmpod,  unordered, !)
 
-GENERATE_CVT_FLOAT1_OP(fstois,   float32, int32,   32, 32, make_float32, no_cvt)
-GENERATE_CVT_FLOAT1_OP(istofs,   int32,   float32, 32, 32, no_cvt, float32_val)
-GENERATE_CVT_FLOAT1_OP(fstoistr, float32, int32_round_to_zero, 32, 32, make_float32, no_cvt)
+GENERATE_CVT_FLOAT1_OP(fstois,   float32, int32,   uint32_t, uint32_t, make_float32, no_cvt)
+GENERATE_CVT_FLOAT1_OP(istofs,   int32,   float32, uint32_t, uint32_t, no_cvt, float32_val)
+GENERATE_CVT_FLOAT1_OP(fstoistr, float32, int32_round_to_zero, uint32_t, uint32_t, make_float32, no_cvt)
 
-GENERATE_CVT_FLOAT1_OP(fdtoid,   float64, int64,   64, 64, make_float64, no_cvt)
-GENERATE_CVT_FLOAT1_OP(idtofd,   int64,   float64, 64, 64, no_cvt, float64_val)
-GENERATE_CVT_FLOAT1_OP(fdtoidtr, float64, int64_round_to_zero, 64, 64, make_float64, no_cvt)
+GENERATE_CVT_FLOAT1_OP(fdtoid,   float64, int64,   uint64_t, uint64_t, make_float64, no_cvt)
+GENERATE_CVT_FLOAT1_OP(idtofd,   int64,   float64, uint64_t, uint64_t, no_cvt, float64_val)
+GENERATE_CVT_FLOAT1_OP(fdtoidtr, float64, int64_round_to_zero, uint64_t, uint64_t, make_float64, no_cvt)
 
-GENERATE_CVT_FLOAT1_OP(fstofd,   float32, float64, 32, 64, make_float32, float64_val)
-GENERATE_CVT_FLOAT1_OP(fstoid,   float32, int64,   32, 64, make_float32, no_cvt)
-GENERATE_CVT_FLOAT1_OP(istofd,   int32,   float64, 32, 64, no_cvt, float64_val)
-GENERATE_CVT_FLOAT1_OP(fstoidtr, float32, int64_round_to_zero, 32, 64, make_float32, no_cvt)
+GENERATE_CVT_FLOAT1_OP(fstofd,   float32, float64, uint32_t, uint64_t, make_float32, float64_val)
+GENERATE_CVT_FLOAT1_OP(fstoid,   float32, int64,   uint32_t, uint64_t, make_float32, no_cvt)
+GENERATE_CVT_FLOAT1_OP(istofd,   int32,   float64, uint32_t, uint64_t, no_cvt, float64_val)
+GENERATE_CVT_FLOAT1_OP(fstoidtr, float32, int64_round_to_zero, uint32_t, uint64_t, make_float32, no_cvt)
 
-GENERATE_CVT_FLOAT1_OP(fdtofs,   float64, float32, 64, 32, make_float64, float32_val)
-GENERATE_CVT_FLOAT1_OP(fdtois,   float64, int32,   64, 32, make_float64, no_cvt)
-GENERATE_CVT_FLOAT1_OP(idtofs,   int64,   float32, 64, 32, no_cvt, float32_val)
-GENERATE_CVT_FLOAT1_OP(fdtoistr, float64, int32_round_to_zero, 64, 32, make_float64, no_cvt)
+GENERATE_CVT_FLOAT1_OP(fdtofs,   float64, float32, uint64_t, uint32_t, make_float64, float32_val)
+GENERATE_CVT_FLOAT1_OP(fdtois,   float64, int32,   uint64_t, uint32_t, make_float64, no_cvt)
+GENERATE_CVT_FLOAT1_OP(idtofs,   int64,   float32, uint64_t, uint32_t, no_cvt, float32_val)
+GENERATE_CVT_FLOAT1_OP(fdtoistr, float64, int32_round_to_zero, uint64_t, uint32_t, make_float64, no_cvt)
+
+GENERATE_CVT_FLOAT1_OP(fxtofs,   floatx80, float32, floatx80*, uint32_t, deref, float32_val)
+GENERATE_CVT_FLOAT1_OP(fxtofd,   floatx80, float64, floatx80*, uint64_t, deref, float64_val)
+
+void HELPER(fstofx)(floatx80 *ret, CPUE2KState *env, uint32_t x)
+{
+    uint8_t old_flags = save_exception_flags(env);
+    *ret = float32_to_floatx80(make_float32(x), &env->fp_status);
+    merge_exception_flags(env, old_flags);
+}
+
+void HELPER(fdtofx)(floatx80 *ret, CPUE2KState *env, uint64_t x)
+{
+    uint8_t old_flags = save_exception_flags(env);
+    *ret = float64_to_floatx80(make_float64(x), &env->fp_status);
+    merge_exception_flags(env, old_flags);
+}
+
+#define GEN_OP2_XX(name, op) \
+    void HELPER(name)(CPUE2KState *env, floatx80 *x, floatx80 *y) \
+    { \
+	    uint8_t old_flags = save_exception_flags(env); \
+	    *x = glue(floatx80_, op)(*x, *y, &env->fp_status); \
+	    merge_exception_flags(env, old_flags); \
+    }
+
+GEN_OP2_XX(fxaddxx, add)
+GEN_OP2_XX(fxsubxx, sub)
+GEN_OP2_XX(fxmulxx, mul)
+GEN_OP2_XX(fxdivxx, div)
+
+void HELPER(fxrsubxx)(CPUE2KState *env, floatx80 *x, floatx80 *y)
+{
+    uint8_t old_flags = save_exception_flags(env);
+    *x = floatx80_sub(*y, *x, &env->fp_status);
+    merge_exception_flags(env, old_flags);
+}
