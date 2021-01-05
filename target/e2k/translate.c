fb1c8adcc7b8dcd409ac0f0c57e8e13dffc642ba
Denis Drakhnya <numas13@gmail.com>
2021-01-05 12:47:59 +0200

    e2k: Invalidate mlock after each instruction.

--- target/e2k/translate.c
+++ target/e2k/translate.c
@@ -396,6 +396,7 @@ static void e2k_tr_translate_insn(DisasContextBase *db, CPUState *cs)
     do_commit(ctx);
     do_branch(ctx, pc_next);
 
+    ctx->mlock = NULL;
     ctx->base.pc_next = pc_next;
 
     /* Free temporary values */
