7f3dbf0c0de8e9b487bd9bc736165278e6626fce
Alibek Omarov <a1ba.omarov@gmail.com>
2021-01-08 04:43:09 +0300

    e2k: elbrus by default have extended precision, set it

--- target/e2k/cpu.c
+++ target/e2k/cpu.c
@@ -56,6 +56,9 @@ static void e2k_cpu_reset(DeviceState *dev)
     env->bp.size = 1;
     env->aau.incrs[0] = 1; /* always one */
     env->fpcr._one = 1; 
+    env->fpcr.pc = FPCR_PC_XP;
+    
+    e2k_update_fp_status(env);
 
     // FIXME: testing
     env->idr = 0x3a207; // mimic 8c
