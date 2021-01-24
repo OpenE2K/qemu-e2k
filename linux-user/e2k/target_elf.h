f56cad79232fa6fd9fe09ffd4b4a369f5e8e7d8e
Alibek Omarov <a1ba.omarov@gmail.com>
2021-01-24 06:12:54 +0300

    e2k: implement detecting ISA version by ELF flags, throw an error for protected mode and x86 recompiled binaries for now

--- linux-user/e2k/target_elf.h
+++ linux-user/e2k/target_elf.h
@@ -1,7 +1,27 @@
 #ifndef E2K_TARGET_ELF_H
 #define E2K_TARGET_ELF_H
-static inline const char *cpu_get_model(uint32_t eflags)
-{
-    return "any";
-}
+
+#define E2K_ELF_IPD_MASK  ((1U << 1)|(1U << 0))
+#define E2K_ELF_X86APP    (1U << 2)
+#define E2K_ELF_4MB_PAGES (1U << 3)
+#define E2K_ELF_INCOMPAT  (1U << 4)
+#define E2K_ELF_PM        (1U << 5)
+
+#define E2K_ELF_OLD_MACH(x) (((x) >> 28) & 7)
+#define E2K_ELF_MACH(x)     (((x) >> 24) & 255)
+
+#define E2K_MACH_BASE 0
+#define E2K_MACH_EV1  1
+#define E2K_MACH_EV2  2
+#define E2K_MACH_EV3  3
+#define E2K_MACH_EV4  4
+#define E2K_MACH_EV5  5
+#define E2K_MACH_EV6  6
+
+/* elbrus-v4 based processors */
+#define E2K_MACH_8C     19
+#define E2K_MACH_1CPLUS 20
+
+const char *cpu_get_model(uint32_t eflags);
+
 #endif /* E2K_TARGET_ELF_H */
