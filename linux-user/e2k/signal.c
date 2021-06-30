657cab1e192aac130dca3caba0b87d6a675e2e51
Alibek Omarov <a1ba.omarov@gmail.com>
2021-06-30 15:50:58 +0300

    e2k: pass env to do_sigaltstack
    Signed-off-by: Alibek Omarov <a1ba.omarov@gmail.com>

--- linux-user/e2k/signal.c
+++ linux-user/e2k/signal.c
@@ -336,7 +336,7 @@ long do_rt_sigreturn(CPUE2KState *env)
 
     if (do_sigaltstack(frame_addr +
             offsetof(struct target_sigframe, uc.uc_stack),
-            0, get_sp_from_cpustate(env)) == -EFAULT)
+            0, env) == -EFAULT)
     {
         goto badframe;
     }
